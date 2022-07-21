43e71e2b1f9489e94fcd1ace4a68ddef2e701d58
DanielGomes <50127863+danieldjgomes@users.noreply.github.com>
2022-07-20 21:15:13 -0300

    Atualizacao security com acesso via facebook

--- src/main/java/br/com/gomes/daniel/ufabc/alertadematricula/matriculaapi/framework/configuracao/ConfiguracaoOAuth2.java
+++ src/main/java/br/com/gomes/daniel/ufabc/alertadematricula/matriculaapi/framework/configuracao/ConfiguracaoOAuth2.java
@@ -43,15 +43,7 @@ public class ConfiguracaoOAuth2 extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-
         http
-//                .antMatcher("/**")
-//                .authorizeHttpRequests()
-//                .antMatchers("/", "/login**","/error**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated();
-
                 .authorizeRequests()
                 .antMatchers("/oauth2/**").permitAll()
                 .anyRequest().authenticated()
